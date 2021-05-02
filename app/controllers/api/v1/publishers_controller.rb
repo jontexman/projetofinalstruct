29c7a32ea8d628c9d1cc35fbb30b55e4f855f0fb
Mateus <matthewfla31@gmail.com>
2021-05-02 03:55:54 -0300

    Implementaçao da controller publishers e suas rotas

---
+++ app/controllers/api/v1/publishers_controller.rb
@@ -0,0 +1,73 @@
+class Api::V1::PublishersController < ApplicationController
+    def index
+        publishers  = Publisher.all
+        render json: publishers
+    end
+    
+    def show
+        if Publisher.exists?(params[:id])
+            publisher = Publisher.find(params[:id])
+            render json: publisher, status: :ok
+        else
+            render json: {status: 'ERROR', message: 'falha ao mostrar publicadora, id não existente'}, status: :unprocessable_entity
+        end
+    end
+
+    def create
+        publisher = Publisher.new(publishers_params)
+        if publisher.save
+             render json: publisher 
+        else
+             render json: {status:'ERROR', message: 'falha ao criar publicadora ' }, status: :unprocessable_entity
+        end 
+    end
+
+    def destroy
+        if Publisher.exists?(params[:id])
+            publisher = Publisher.find(params[:id])
+            if publisher.destroy!
+                render json: {status:'SUCESSS', message:'publicadora foi apagada'}, status: :ok
+            else
+                render json: {status:'ERROR', message:'falha ao apagar publicadora'}, status: :bad_request
+            end
+        else
+            render json: {status: 'ERROR', message: 'falha ao apagar publicadora, id não existente'}, status: :bad_request
+
+        end
+        
+    end
+
+    def update
+        if Publisher.exists?(params[:id])
+            publisher = Publisher.find(params[:id])
+            if publisher.update(publishers_params)
+                render json: publisher, status: :ok
+            else
+                render json: {status: 'ERROR', message: 'falha ao editar publicadora', data: publisher}, status: :bad_request 
+            end    
+        else
+            render json: {status: 'ERROR', message: 'falha ao editar publicadora, id não existente'}, status: :bad_request
+        end    
+
+    end
+
+    
+     '''
+    def games_publishers
+        if Publisher.exists?(params[:id])
+            publisher = Publisher.find(params[:id])
+            render json: {name:Publisher.name, games:Publisher.games}, status: :ok 
+        
+        else
+            render json: {status: 'ERROR', message: 'falha ao mostrar publicadora, id não existente'}, status: :unprocessable_entity
+        end    
+    end
+    '''  
+    
+
+    private
+    def publishers_params
+        params.require(:publisher).permit(:name)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      scope 'publishers/' do
+        get 'index', to:'publishers#index', as: 'publisher_index'
+        get 'show/:id', to:'publishers#show', as: 'show_publisher'
+        post 'create', to:'publishers#create', as: 'create_publisher'
+        delete 'destroy/:id', to:'publishers#destroy', as: 'destroy_publisher'
+        put 'update/:id', to:'publishers#update', as: 'update_publisher'
+        get 'games_publishers/:id', to:'publishers#games_publishers', as: 'publishers_games'
+      end
+    end
+  end
 end
