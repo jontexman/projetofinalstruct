caf0b54955bb9e3c1ef8f6516828d761ec7290af
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-03 23:39:45 -0300

    image adicionada a model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :name,presence: true
   validates :email,presence: true
   validates :password,presence: true
+
+  has_one_attached :image
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
