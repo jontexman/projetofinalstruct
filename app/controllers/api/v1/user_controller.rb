61f8dc2b04436d549d0207430c31dc76583f864a
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-04 15:32:10 -0300

    sign in adicionado a controller

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UserController < ApplicationController
+
+    def sign_in
+        user = User.find_by(email: params[:email])
+
+        if user.valid_password?(params[:password])
+            render json: user.as_json(only: %i[username email authentication_token])
+        else
+            head(status :unauthorazied)
+        end
+    end
+
+    def index
+        
+    end
+end
