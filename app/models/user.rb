a2ce76112b7742e6aa38329e9d7df73de02c26da
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-03 22:00:33 -0300

    validacoes model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name,presence: true
+  validates :email,presence: true
+  validates :password,presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
