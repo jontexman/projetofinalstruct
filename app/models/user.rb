7351142439234090a555fc2e0633d73d4e7e95d1
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-03 22:31:11 -0300

    Token de autenticacao adicionado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_token_authenticatable
+  
   validates :name,presence: true
   validates :email,presence: true
   validates :password,presence: true
