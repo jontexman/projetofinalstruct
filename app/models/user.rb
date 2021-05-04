563bc365169f1d0d36304ee79206cc9e2051a77f
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-03 22:32:57 -0300

    Adicionado uniqueness na validaçao do usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_token_authenticatable
-  
-  validates :name,presence: true
-  validates :email,presence: true
-  validates :password,presence: true
+
+  validates :name,presence: true, uniqueness: true
+  validates :email,presence: true, uniqueness: true
+  validates :password,presence: true, uniqueness: true
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
