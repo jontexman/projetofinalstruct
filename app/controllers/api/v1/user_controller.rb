11f4f1a27bef58d4700a45d9d7d4bfd4be3fb8c3
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-03 22:10:39 -0300

    validacao de senha

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::UserController < ApplicationController
+
+    def sign_in
+        user = User.find_by(email: params[:email])
+
+        if user.valid_password?(params[:password])
+            render json: user.as_json(only: %i[username email])
+    end
 end
