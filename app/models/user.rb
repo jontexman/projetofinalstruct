fd836fedb6e57e1a9d8f7e9c5dd149a805545a42
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-06 11:47:08 -0300

    adicionado jogos a model do usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :games
+
   validates :name,presence: true
   validates :email,presence: true
   validates :password,presence: true
