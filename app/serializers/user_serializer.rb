628bea531d53ecd83ae3ec3bb07943232dbf8a50
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-07 00:46:30 -0300

    Serializer adicionada para o usuario

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,7 +3,7 @@ include Rails.application.routes.url_helpers
 
   attributes :id, :name, :profile_pic, :email
 
-  def image_url
+  def profile_pic
     if object.image.attached?
       rails_blob_path(object.image, only_path: true)
     else
