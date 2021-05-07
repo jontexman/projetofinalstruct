62baf9a5b83104ea4a2f42e7dadf5d14b9a6d0f5
Mateus <matthewfla31@gmail.com>
2021-05-07 11:56:26 -0300

    tabela_associativa_game_users+biblioteca

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ApplicationRecord
   validates :name, presence: true
   has_many :gamecategories
   has_many :categories, through: :gamecategories
+  has_many :users, through: :game_user
+  has_many :game_users
 end
