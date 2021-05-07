413fbc183cad41c088c13571dc6b273d60737012
Pedro Lucas <pedroluksteixeira@gmail.com>
2021-05-06 22:42:06 -0300

    ajustes nos metodos destroy e update do user

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,20 @@ Rails.application.routes.draw do
       scope 'users' do
         post 'create', to: 'user#create', as: 'create_user'
         post 'login', to: 'user#login', as: 'login_user'
-        delete 'destroy', to: 'user#destroy', as:'delete_user'
-        put 'update', to: 'user#update', as:'update_user'
+        delete 'destroy/:id', to: 'user#destroy', as:'delete_user'
+        put 'update/:id', to: 'user#update', as:'update_user'
         get 'show/:id', to:'user#show', as:'show_user'
       end
+
+      scope 'publishers/' do
+        get 'index', to:'publishers#index', as: 'publisher_index'
+        get 'show/:id', to:'publishers#show', as: 'show_publisher'
+        post 'create', to:'publishers#create', as: 'create_publisher'
+        delete 'destroy/:id', to:'publishers#destroy', as: 'destroy_publisher'
+        put 'update/:id', to:'publishers#update', as: 'update_publisher'
+        get 'games_publishers/:id', to:'publishers#games_publishers', as: 'publishers_games'
+      end
+
     end
   end
 end
